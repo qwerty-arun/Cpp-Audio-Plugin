c9f06ddc40aab27fca3605c862e8ff412b173d01
qwerty-arun <arunkr.1804@gmail.com>
2025-10-20 10:16:21 +0530

    updated DSP from params

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -506,7 +506,9 @@ void CAudioPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
 
     //[DONE]: add APVTs
     //[DONE]: create audio parameters for all dsp choices
-    //TODO: update DSP here from audio parameters
+    //[DONE]: update DSP here from audio parameters
+    //TODO: update general filter coefficients
+    //TODO: add smoothers for all param updates
     //[DONE]: save/load settings
     //TODO: save/load DSP order
     //TODO: Drag-To-Recorder GUI
@@ -519,6 +521,25 @@ void CAudioPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
     //TODO: thread-safe filter updating [BONUS]
     //TODO: pre/post filtering [BONUS]
     //TODO: delay module [BONUS]
+
+    phaser.dsp.setRate( phaserRateHz->get() );
+    phaser.dsp.setCentreFrequency( phaserCenterFreqHz->get() );
+    phaser.dsp.setDepth( phaserDepthPercent->get() );
+    phaser.dsp.setFeedback( phaserFeedbackPercent->get() );
+    phaser.dsp.setMix(phaserMixPercent->get());
+
+    chorus.dsp.setRate( chorusRateHz->get() );
+    chorus.dsp.setDepth( chorusDepthPercent->get() );
+    chorus.dsp.setCentreDelay( chorusCenterDelayMs->get() );
+    chorus.dsp.setFeedback( chorusFeedbackPercent->get() );
+    chorus.dsp.setMix( chorusMixPercent->get() );
+
+    overdrive.dsp.setDrive( overdriveSaturation->get() );
+
+    ladderFilter.dsp.setMode( static_cast<juce::dsp::LadderFilterMode>(ladderFilterMode->getIndex()) );
+    ladderFilter.dsp.setCutoffFrequencyHz( ladderFilterCutoffHz->get() );
+    ladderFilter.dsp.setResonance( ladderFilterResonance->get() );
+    ladderFilter.dsp.setDrive( ladderFilterDrive->get() );
 
     // default instance
     auto newDSPOrder = DSP_Order();
